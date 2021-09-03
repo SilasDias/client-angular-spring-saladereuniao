bfae592022ef74588e0e0a65699223a31f44e3ce
silas.dias <silasdjd@gmail.com>
2021-09-03 18:49:00 -0300

    Projeto iniciado

---
+++ src/test/java/dias/silas/crud/saladereuniao/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dias.silas.crud.saladereuniao.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
